1ac959736bdf55230acf959f6a353b7a5fe9766f
Rafael Coelho <rafaellcoellho@gmail.com>
2023-05-20 14:51:30 -0300

    adiciona botão para visualizar mensagem de usuário

--- gerenciador_espaco_tuplas/interface/interface_janela_ambientes.py
+++ gerenciador_espaco_tuplas/interface/interface_janela_ambientes.py
@@ -6,17 +6,17 @@ class InterfaceJanelaAmbientes(tkinter.Frame):
         super().__init__(janela)
         self.grid(row=0, column=0, padx=10, pady=10)
 
-        # self.frame_janela_ambientes: tkinter.Frame = tkinter.Frame(self)
-        # self.frame_janela_ambientes.grid(row=0, column=0, padx=10, pady=10)
-        # self.frame_janela_ambientes.grid_columnconfigure(0, weight=1)
-        # self.frame_janela_ambientes.grid_columnconfigure(1, weight=1)
-
         self.frame_gerenciador_usuarios: tkinter.LabelFrame = tkinter.LabelFrame(
             self,
             text="Usuários",
         )
         self.mostrador_de_usuarios: tkinter.Listbox = tkinter.Listbox(
             self.frame_gerenciador_usuarios,
+        )
+        self.botao_mensagens_do_usuario: tkinter.Button = tkinter.Button(
+            self.frame_gerenciador_usuarios,
+            text="Mensagens",
+            command=self._ver_mensagens_do_usuario,
         )
         self.botao_mover_usuario_para_ambiente: tkinter.Button = tkinter.Button(
             self.frame_gerenciador_usuarios,
@@ -53,12 +53,14 @@ class InterfaceJanelaAmbientes(tkinter.Frame):
         self.frame_gerenciador_usuarios.grid(row=0, column=0)
         self.frame_gerenciador_usuarios.grid_columnconfigure(0, weight=1)
         self.frame_gerenciador_usuarios.grid_columnconfigure(1, weight=1)
+        self.frame_gerenciador_usuarios.grid_columnconfigure(2, weight=1)
 
         self.mostrador_de_usuarios.grid(
-            row=0, column=0, columnspan=2, sticky=tkinter.EW
+            row=0, column=0, columnspan=3, sticky=tkinter.EW
         )
         self.botao_mover_usuario_para_ambiente.grid(row=1, column=0, sticky=tkinter.EW)
         self.botao_excluir_usuario.grid(row=1, column=1, sticky=tkinter.EW)
+        self.botao_mensagens_do_usuario.grid(row=1, column=2, sticky=tkinter.EW)
 
     def _configurar_interface_gerenciador_dispositivos(self):
         self.frame_gerenciador_dispositivos.grid(row=0, column=1)
@@ -72,6 +74,9 @@ class InterfaceJanelaAmbientes(tkinter.Frame):
             row=1, column=0, sticky=tkinter.EW
         )
         self.botao_excluir_dispositivo.grid(row=1, column=1, sticky=tkinter.EW)
+
+    def _ver_mensagens_do_usuario(self):
+        pass
 
     def _mover_usuario_para_ambiente(self):
         pass
