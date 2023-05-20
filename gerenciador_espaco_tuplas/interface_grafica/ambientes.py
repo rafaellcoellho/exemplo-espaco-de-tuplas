304d1eed8539f499a459ffec0bc3046651a7a406
Rafael Coelho <rafaellcoellho@gmail.com>
2023-05-20 19:09:21 -0300

    Interface gráfica sem implementação de espaço de tuplas (#3)
    * implementa interface de criação de tuplas
    
    * transforma gerenciador geral de adicionar nova tupla em gerenciador
    apenas para os ambientes
    
    * adiciona entry e botões para visualizar ambientes
    
    * corrige posicionamento dos itens na interface
    
    * cria classe para gerenciar a janela de ambiente
    
    * adiciona botão para visualizar mensagem de usuário
    
    * implementa layout da tela de visualização de mensagens do usuário
    
    * muda implementação para herdar de TopLevel ao invés de Frame
    
    * melhora organização dos módulos e nomeclatura
    
    * cria janela de movimentação entre ambientes

---
+++ gerenciador_espaco_tuplas/interface_grafica/ambientes.py
@@ -0,0 +1,84 @@
+import tkinter
+from tkinter import messagebox
+
+from gerenciador_espaco_tuplas.interface_grafica.janelas.ambiente import (
+    JanelaDeAmbiente,
+)
+
+
+class InterfaceGerenciadorDeAmbientes(tkinter.LabelFrame):
+    def __init__(self, frame_pai: tkinter.Frame):
+        super().__init__(frame_pai, text="Gerenciador de Ambientes")
+
+        self.frame_gerenciador_ambientes: tkinter.Frame = tkinter.Frame(self)
+        self.frame_gerenciador_ambientes.grid(row=0, column=0, padx=10, pady=10)
+
+        self.frame_adicionar_ambiente: tkinter.Frame = tkinter.Frame(
+            self.frame_gerenciador_ambientes
+        )
+        self.entrada_nome_novo_ambiente: tkinter.Entry = tkinter.Entry(
+            self.frame_adicionar_ambiente,
+            font=("Arial", 15),
+        )
+        self.botao_adicionar_novo_ambiente: tkinter.Button = tkinter.Button(
+            self.frame_adicionar_ambiente,
+            text="Adicionar",
+            command=self._adicionar_ambiente,
+        )
+        self._configurar_interface_adicionar_novo_ambiente()
+
+        self.frame_mostrador_de_ambientes: tkinter.Frame = tkinter.Frame(
+            self.frame_gerenciador_ambientes,
+        )
+        self.mostrador_de_ambientes: tkinter.Listbox = tkinter.Listbox(
+            self.frame_mostrador_de_ambientes,
+        )
+        self.botao_ver_ambiente_selecionado: tkinter.Button = tkinter.Button(
+            self.frame_mostrador_de_ambientes,
+            text="Ver",
+            command=self._ver_ambiente_selecionado,
+        )
+        self.botao_excluir_ambiente_selecionado: tkinter.Button = tkinter.Button(
+            self.frame_mostrador_de_ambientes,
+            text="Excluir",
+            command=self._excluir_ambiente_selecionado,
+        )
+        self._configurar_interface_mostrador_de_ambientes()
+
+    def _configurar_interface_adicionar_novo_ambiente(self):
+        self.frame_adicionar_ambiente.grid(row=0, column=0)
+        self.frame_adicionar_ambiente.grid_columnconfigure(0, weight=1)
+
+        self.entrada_nome_novo_ambiente.grid(row=0, column=0)
+        self.botao_adicionar_novo_ambiente.grid(row=0, column=1)
+
+    def _configurar_interface_mostrador_de_ambientes(self):
+        self.frame_mostrador_de_ambientes.grid(row=1, column=0, sticky=tkinter.EW)
+        self.frame_mostrador_de_ambientes.grid_rowconfigure(0, weight=1)
+        self.frame_mostrador_de_ambientes.grid_columnconfigure(0, weight=1)
+        self.frame_mostrador_de_ambientes.grid_columnconfigure(1, weight=1)
+
+        self.mostrador_de_ambientes.grid(
+            row=0, column=0, columnspan=2, sticky=tkinter.EW
+        )
+        self.botao_ver_ambiente_selecionado.grid(row=1, column=0, sticky=tkinter.EW)
+        self.botao_excluir_ambiente_selecionado.grid(row=1, column=1, sticky=tkinter.EW)
+
+    def _adicionar_ambiente(self):
+        nome_novo_ambiente: str = self.entrada_nome_novo_ambiente.get()
+
+        if nome_novo_ambiente == "":
+            messagebox.showerror("Erro", "O nome do ambiente não pode ser vazio.")
+            return
+
+        self.entrada_nome_novo_ambiente.delete(0, tkinter.END)
+        print(nome_novo_ambiente)
+
+    def _ver_ambiente_selecionado(self):
+        ambiente_selecionado: str = self.mostrador_de_ambientes.curselection()
+        JanelaDeAmbiente()
+        print(ambiente_selecionado)
+
+    def _excluir_ambiente_selecionado(self):
+        ambiente_selecionado: str = self.mostrador_de_ambientes.curselection()
+        print(ambiente_selecionado)
---
+++ gerenciador_espaco_tuplas/interface_grafica/janelas/ambiente.py
@@ -0,0 +1,107 @@
+import tkinter
+
+from gerenciador_espaco_tuplas.interface_grafica.janelas.chat import (
+    JanelaDeChat,
+)
+from gerenciador_espaco_tuplas.interface_grafica.janelas.mover_entre_ambientes import (
+    JanelaMoverEntreAmbientes,
+)
+
+
+class JanelaDeAmbiente(tkinter.Toplevel):
+    def __init__(self):
+        super().__init__()
+        self.title("Ambiente")
+
+        self.interface_janela: tkinter.Frame = tkinter.Frame(self)
+        self.interface_janela.grid(row=0, column=0, padx=10, pady=10)
+
+        self.frame_gerenciador_usuarios: tkinter.LabelFrame = tkinter.LabelFrame(
+            self.interface_janela,
+            text="Usuários",
+        )
+        self.mostrador_de_usuarios: tkinter.Listbox = tkinter.Listbox(
+            self.frame_gerenciador_usuarios,
+        )
+        self.botao_mensagens_do_usuario: tkinter.Button = tkinter.Button(
+            self.frame_gerenciador_usuarios,
+            text="Mensagens",
+            command=self._ver_mensagens_do_usuario,
+        )
+        self.botao_mover_usuario_para_ambiente: tkinter.Button = tkinter.Button(
+            self.frame_gerenciador_usuarios,
+            text="Mover",
+            command=self._mover_usuario_para_ambiente,
+        )
+        self.botao_excluir_usuario: tkinter.Button = tkinter.Button(
+            self.frame_gerenciador_usuarios,
+            text="Excluir",
+            command=self._excluir_usuario,
+        )
+        self._configurar_interface_gerenciador_usuarios()
+
+        self.frame_gerenciador_dispositivos: tkinter.LabelFrame = tkinter.LabelFrame(
+            self.interface_janela,
+            text="Dispositivos",
+        )
+        self.mostrador_de_dispositivos: tkinter.Listbox = tkinter.Listbox(
+            self.frame_gerenciador_dispositivos,
+        )
+        self.botao_mover_dispositivo_para_ambiente: tkinter.Button = tkinter.Button(
+            self.frame_gerenciador_dispositivos,
+            text="Mover",
+            command=self._mover_dispositivo_para_ambiente,
+        )
+        self.botao_excluir_dispositivo: tkinter.Button = tkinter.Button(
+            self.frame_gerenciador_dispositivos,
+            text="Excluir",
+            command=self._excluir_dispositivo,
+        )
+        self._configurar_interface_gerenciador_dispositivos()
+
+    def _configurar_interface_gerenciador_usuarios(self):
+        self.frame_gerenciador_usuarios.grid(row=0, column=0)
+        self.frame_gerenciador_usuarios.grid_columnconfigure(0, weight=1)
+        self.frame_gerenciador_usuarios.grid_columnconfigure(1, weight=1)
+        self.frame_gerenciador_usuarios.grid_columnconfigure(2, weight=1)
+
+        self.mostrador_de_usuarios.grid(
+            row=0, column=0, columnspan=3, sticky=tkinter.EW
+        )
+        self.botao_mover_usuario_para_ambiente.grid(row=1, column=0, sticky=tkinter.EW)
+        self.botao_excluir_usuario.grid(row=1, column=1, sticky=tkinter.EW)
+        self.botao_mensagens_do_usuario.grid(row=1, column=2, sticky=tkinter.EW)
+
+    def _configurar_interface_gerenciador_dispositivos(self):
+        self.frame_gerenciador_dispositivos.grid(row=0, column=1)
+        self.frame_gerenciador_dispositivos.grid_columnconfigure(0, weight=1)
+        self.frame_gerenciador_dispositivos.grid_columnconfigure(1, weight=1)
+
+        self.mostrador_de_dispositivos.grid(
+            row=0, column=0, columnspan=2, sticky=tkinter.EW
+        )
+        self.botao_mover_dispositivo_para_ambiente.grid(
+            row=1, column=0, sticky=tkinter.EW
+        )
+        self.botao_excluir_dispositivo.grid(row=1, column=1, sticky=tkinter.EW)
+
+    def _ver_mensagens_do_usuario(self):
+        usuario_selecionado: str = self.mostrador_de_usuarios.curselection()
+        JanelaDeChat()
+        print(usuario_selecionado)
+
+    def _mover_usuario_para_ambiente(self):
+        usuario_selecionado: str = self.mostrador_de_usuarios.curselection()
+        JanelaMoverEntreAmbientes()
+        print(usuario_selecionado)
+
+    def _excluir_usuario(self):
+        pass
+
+    def _mover_dispositivo_para_ambiente(self):
+        dispositivo_selecionado: str = self.mostrador_de_dispositivos.curselection()
+        JanelaMoverEntreAmbientes()
+        print(dispositivo_selecionado)
+
+    def _excluir_dispositivo(self):
+        pass
